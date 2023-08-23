d489b35fc2a8f9b3424b498b39e95af832032c30
mohitx1 <mohitrockx3@gmail.com>
2023-08-23 14:29:34 +0530

    setting data converting to string using JSONstringify

--- index.js
+++ index.js
@@ -11,4 +11,14 @@ document.cookie = 'Name=Sinha; expires=' + new Date(2024, 0, 1).toUTCString();
 console.log(localStorage.Name)
 console.log(document.cookie)
 
+const object={
+    Name:'Mohit',
+    Email:'mohit@gmail.com'
+}
+
+const objectSerialized=JSON.stringify(object);
+localStorage.setItem('myObject',objectSerialized);
+
+const objectDeserialized=JSON.parse(localStorage.getItem('myObject'));
+console.log(objectDeserialized)
 
